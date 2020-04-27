d47d6d9ddac70e52c83fdde2ec8c57ce693661ff
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-27 20:53:39 +0430

    initial components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 
+import CommentBox from "./CommentBox";
+import CommentList from "./CommentList";
+
 class App extends Component {
     render() {
         return (
             <div>
-                App
+                <CommentBox/>
+                <CommentList/>
             </div>
         );
     }
