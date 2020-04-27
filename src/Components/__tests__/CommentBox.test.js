fb2dba03e50b72b4617fe678d4bc261c4d4df93f
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-28 01:01:39 +0430

    use describe function

--- src/Components/__tests__/CommentBox.test.js
+++ src/Components/__tests__/CommentBox.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import {mount} from "enzyme";
 
 import CommentBox from "../CommentBox";
+
 let wrappedCommentBox;
 
 beforeEach(() => {
@@ -22,34 +23,27 @@ it('has a button.', doesCommentBoxContain('button'));
 
 it('has a header.', doesCommentBoxContain('h4'));
 
-it('has a textarea that users can type in.', () => {
+describe('the textarea update', () => {
     const newCommentValue = 'new comment';
+    beforeEach(() => {
+        wrappedCommentBox.find('textarea').simulate('change', {
+            target: {
+                value: newCommentValue
+            }
+        });
 
-    wrappedCommentBox.find('textarea').simulate('change', {
-        target: {
-            value: newCommentValue
-        }
+        wrappedCommentBox.update();
     });
 
-    wrappedCommentBox.update();
-
-    expect(wrappedCommentBox.find('textarea').prop('value')).toEqual(newCommentValue);
-});
-
-it('has a form that submit successfully', () => {
-    const newCommentValue = 'new comment';
-
-    wrappedCommentBox.find('textarea').simulate('change', {
-        target: {
-            value: newCommentValue
-        }
+    it('has a textarea that users can type in.', () => {
+        expect(wrappedCommentBox.find('textarea').prop('value')).toEqual(newCommentValue);
     });
 
-    wrappedCommentBox.update();
+    it('has a form that submit successfully', () => {
+        wrappedCommentBox.find('form').simulate('submit');
 
-    wrappedCommentBox.find('form').simulate('submit');
+        wrappedCommentBox.update();
 
-    wrappedCommentBox.update();
-
-    expect(wrappedCommentBox.find('textarea').prop('value')).toEqual('');
+        expect(wrappedCommentBox.find('textarea').prop('value')).toEqual('');
+    });
 });
