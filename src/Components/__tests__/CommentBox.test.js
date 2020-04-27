19e2480c7ca820c03cc0ac55be2211421c626c61
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-28 01:30:40 +0430

    handle testing when using redux

--- src/Components/__tests__/CommentBox.test.js
+++ src/Components/__tests__/CommentBox.test.js
@@ -1,12 +1,17 @@
 import React from "react";
 import {mount} from "enzyme";
 
+import Root from "../../Root";
 import CommentBox from "../CommentBox";
 
 let wrappedCommentBox;
 
 beforeEach(() => {
-    wrappedCommentBox = mount(<CommentBox/>);
+    wrappedCommentBox = mount(
+        <Root>
+            <CommentBox/>
+        </Root>
+    );
 });
 
 afterEach(() => {
