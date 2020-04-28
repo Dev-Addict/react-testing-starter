9f0bbd7691f2bb501e12cab1e39526474eac519a
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-28 11:22:39 +0430

    write header logic for app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import CommentBox from "./CommentBox";
 import CommentList from "./CommentList";
+import {changeAuth} from "../actions";
 
 class App extends Component {
+    renderAuthButton() {
+        const buttonText = this.props.auth?'Log Out':'Sign In';
+        return (
+            <Link>
+                <button onClick={this.props.changeAuth}>
+                    {buttonText}
+                </button>
+            </Link>
+        );
+    }
+
+    renderHeader() {
+        return (
+            <div className="app-header">
+                <Link to="/">Home</Link>
+                <Link to={"/post"}>Post</Link>
+                {this.renderAuthButton()}
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
+                {this.renderHeader()}
                 <Route path="/" component={CommentList}/>
                 <Route path="/post" component={CommentBox}/>
             </div>
@@ -15,4 +39,10 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        auth: state.auth
+    };
+};
+
+export default connect(mapStateToProps, {changeAuth})(App);
